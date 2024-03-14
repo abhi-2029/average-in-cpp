38644809ed83da60837b6db29ef3b6f6a65f6508
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-03-14 19:39:27 +0530

    Add files via upload

---
+++ Average/avg.cpp
@@ -0,0 +1,48 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     int avg;
+//     cout<<"Enter the value of a: ";
+//     cin>>a;
+//     cout<<"Enter the value of b: ";
+//     cin>>b;
+//     cout<<"Enter the vaklue of c: ";
+//     cin>>c;
+//     avg=(a+b+c)/3;
+//     cout<<"AVG IS : "<<avg<<endl;
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=12;
+//     int b=56;
+//      int c=45;
+//      int avg=0;
+//      avg=(a+b+c)/3;
+//      cout<<"AVG IS :"<<avg<<endl;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     float average;
+//     cout<<"Enter the value of a: ";
+//     cin>>a;
+//     cout<<"Enter the value of b: ";
+//     cin>>b;
+//     cout<<"Enter the value of c: " ;
+//     cin>>c;
+//     float sum=a+b+c;
+//     average=sum/3;
+//     cout<<"Sum of three numbers: "<<sum<<endl;
+//     cout<<"Average of three numbers: "<<average<<endl;
+//     return 0;
+// }
